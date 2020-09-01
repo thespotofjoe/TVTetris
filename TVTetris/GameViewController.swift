8ba148ed6c2b0923aa6b909c186f7f257f735359
Joe <joebuchoff@gmail.com>
2020-08-31 20:01:04 -0500

    Initial Commit

---
+++ TVTetris/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  TVTetris
+//
+//  Created by Joseph Buchoff on 8/31/20.
+//  Copyright © 2020 Joe's Studio. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
